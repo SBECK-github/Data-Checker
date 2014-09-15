6f48cb1a6a69ad17f0189850b2d6ea9ec37a242d
Sullivan Beck <sbeck@cpan.org>
2014-09-15 07:54:01 -0400

    Initial checkin of next release cycle: 1.05

--- lib/Data/Checker.pm
+++ lib/Data/Checker.pm
@@ -12,7 +12,7 @@ use Module::Loaded;
 use Parallel::ForkManager 0.7.6;
 
 our($VERSION);
-$VERSION='1.04';
+$VERSION='1.05';
 
 ###############################################################################
 # BASE METHODS
