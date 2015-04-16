5edf4fe6fbc40024a1621b096fe4f67eb4d91efd
Sullivan Beck <sbeck@cpan.org>
2015-04-16 11:07:59 -0400

    Release: 1.06

--- lib/Data/Checker/Ping.pm
+++ lib/Data/Checker/Ping.pm
@@ -1,5 +1,5 @@
 package Data::Checker::Ping;
-# Copyright (c) 2013-2014 Sullivan Beck. All rights reserved.
+# Copyright (c) 2013-2015 Sullivan Beck. All rights reserved.
 # This program is free software; you can redistribute it and/or modify it
 # under the same terms as Perl itself.
 
